33693541da04b8834e792bd61aa5ce3e1f6ec08b
Logan <146642263+logan2d5@users.noreply.github.com>
2024-07-07 19:11:34 +0800

    chore: init vue-todo

---
+++ vue-todo/src/components/Test.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+</script>
+
+<template>
+  <li v-for="item in items">
+    {{ item.message }}
+  </li>
+</template>
+
+<style scoped></style>
